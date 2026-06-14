b724df2978eed6ba9408f8ed0933fb5d4b16cc99
AdeyinkaEmmanuel08 <adeyinkaemmanuel208@gmail.com>
2026-06-14 20:55:43 +0100

    14/06/2025

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// Converting Fahrenheit to Celsius
-function fahrenheitToCelsius(fahrenheit) {
-  let celsius = ((fahrenheit - 32) * 5) / 9;
-  console.log(celsius);
+function checkEntry(age, isVIP) {
+  if (isVIP === true) {
+    console.log("Access Granted: Welcome VIP!");
+  } else if (age < 21) {
+    console.log("Access Denied: Too young!");
+  } else {
+    console.log("Access Granted: Enjoy the night!");
+  }
 }
 
-// Converting Celsius to Fahrenheit
-function celsiusToFahrenheit(celsius) {
-  let fahrenheit = (celsius * 9) / 5 + 32;
-  console.log(fahrenheit);
-}
+// // Converting Fahrenheit to Celsius
+// function fahrenheitToCelsius(fahrenheit) {
+//   let celsius = ((fahrenheit - 32) * 5) / 9;
+//   console.log(celsius);
+// }
+
+// // Converting Celsius to Fahrenheit
+// function celsiusToFahrenheit(celsius) {
+//   let fahrenheit = (celsius * 9) / 5 + 32;
+//   console.log(fahrenheit);
+// }
+
+// // Example calls
+// fahrenheitToCelsius(100); // logs 37.78
+// celsiusToFahrenheit(0); // logs 32

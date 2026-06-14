acc434c22a1090c89215fa99e4309e7fb8fca5e1
AdeyinkaEmmanuel08 <adeyinkaemmanuel208@gmail.com>
2026-06-14 20:59:37 +0100

    14/6/2026

--- index.js
+++ index.js
@@ -7,19 +7,3 @@ function checkEntry(age, isVIP) {
     console.log("Access Granted: Enjoy the night!");
   }
 }
-
-// // Converting Fahrenheit to Celsius
-// function fahrenheitToCelsius(fahrenheit) {
-//   let celsius = ((fahrenheit - 32) * 5) / 9;
-//   console.log(celsius);
-// }
-
-// // Converting Celsius to Fahrenheit
-// function celsiusToFahrenheit(celsius) {
-//   let fahrenheit = (celsius * 9) / 5 + 32;
-//   console.log(fahrenheit);
-// }
-
-// // Example calls
-// fahrenheitToCelsius(100); // logs 37.78
-// celsiusToFahrenheit(0); // logs 32

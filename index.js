000f182d89cfc58d1f7310b321820ab7ec5e932b
AdeyinkaEmmanuel08 <adeyinkaemmanuel208@gmail.com>
2026-06-15 08:19:56 +0100

    ..

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function checkEntry(age, isVIP) {
   if (isVIP === true) {
-    console.log("Access Granted: Welcome VIP!");
-  } else if (age < 21) {
-    console.log("Access Denied: Too young!");
-  } else {
-    console.log("Access Granted: Enjoy the night!");
+    return "Access Granted: Welcome VIP!";
   }
+
+  if (age < 21) {
+    return "Access Denied: Too young!";
+  }
+
+  return "Access Granted: Enjoy the night!";
 }
+
+// function checkEntry(age, isVIP) {
+//   if (isVIP === true) {
+//     console.log("Access Granted: Welcome VIP!");
+//   } else if (age < 21) {
+//     console.log("Access Denied: Too young!");
+//   } else {
+//     console.log("Access Granted: Enjoy the night!");
+//   }
+// }
